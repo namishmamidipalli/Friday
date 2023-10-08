58af31cdcc8aaae45c442eca98619398c3cbb2ee
namishiiit <namish.mamidipalli@students.iiit.ac.in>
2023-10-08 19:06:11 -0400

    Initial Commit

---
+++ Friday/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Friday
+//
+//  Created by Namish Mamidipalli on 10/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Friday/FridayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridayApp.swift
+//  Friday
+//
+//  Created by Namish Mamidipalli on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FridayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
